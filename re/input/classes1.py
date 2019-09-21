9532c1e9f20b1d277dfa7d4458856aed58bc23f3
keni-chi <neko9dog9access@gmail.com>
2019-09-22 00:01:11 +0900

    commit

--- re/input/classes1.py
+++ re/input/classes1.py
@@ -1,16 +1,16 @@
-class HogeAbcDefGGhiJkl1Mno2(object):
+class Hoge1AbcDDefGhi8Jk9l(object):
 
     def __init__(self):
         self.name = 'hoge'
 
-    def get_name(self):
+    def get_name1(self):
         print(self.name)
 
-class FugaAbcDefGGhiJkl1Mno2(object):
+class Fuga1AbcDDefGhi8Jk9l(object):
 
     def __init__(self):
         self.name = 'fuga'
 
-    def get_name(self):
+    def get_name1(self):
         print(self.name)
         
---
+++ re/tool.py
@@ -0,0 +1,54 @@
+import re
+import glob
+
+
+def read_file(file_path):
+    test_data = open(file_path, "r")
+    lines = test_data.readlines()
+    test_data.close()
+    return lines
+
+
+def get_file_path_list():
+    file_path_list = glob.glob('./input/*.py')
+    return file_path_list
+
+
+def camel_to_word(camel):
+    word_list = [x for x in re.split('([a-z]+)([A-Z][a-z0-9]+)|([A-Z][a-z0-9]+)', camel) if x != None and x != '']
+    # print(word_list)
+    return word_list
+
+
+def make_word_list(target):
+    file_path_list = get_file_path_list()
+    
+    for file_path in file_path_list:
+        lines = read_file(file_path)
+
+        matching_str = target + ' '
+        method_list = []
+        for line in lines:
+            if matching_str in line:
+                line = line.replace(matching_str, '')
+                line =  line.split('(')[0]
+                camel = line.replace(' ', '')
+                method_list.extend(camel_to_word(camel))
+
+        words = '\n'.join(method_list)
+
+        file_name = file_path.split('/')[2]
+        file_name = file_name.split('.')[0]
+
+        output_name = './output/' + file_name + '_' + target + '.txt'
+        with open(output_name, 'wt') as f:
+            f.write(words)
+
+
+def main():
+    make_word_list('class')
+    make_word_list('def')
+
+
+if __name__ == '__main__':
+    main()
